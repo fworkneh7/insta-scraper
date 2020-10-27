85bcf5d2c8a2de5f15ac509e920fd5f9e25c9c93
fworkneh7 <fworkneh@umd.edu>
2020-10-27 11:36:04 -0400

    updated website

--- index.py
+++ index.py
@@ -2,4 +2,4 @@ from selenium import webdriver
 
 DRIVER_PATH = "C:/Users/frazer/Downloads/chromedriver_win32/chromedriver.exe" #when working from your local server use your own chromedriver path
 driver = webdriver.Chrome(DRIVER_PATH)
-driver.get('https://google.com')
+driver.get('https://www.instagram.com/p/CGQM1xdB5fW/')
