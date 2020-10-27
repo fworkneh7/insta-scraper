48e06f60b0398d753c3b63730d68205177af3ce0
fworkneh7 <fworkneh@umd.edu>
2020-10-27 15:06:30 -0400

    major logic update

--- index.py
+++ index.py
@@ -1,19 +1,18 @@
 from selenium import webdriver
 
-DRIVER_PATH = "C:/Users/frazer/Downloads/chromedriver_win32/chromedriver.exe" #when working from your local server use your own chromedriver path
+DRIVER_PATH = "C:/Users/frazer/Downloads/chromedriver_win32/chromedriver.exe"    #when working from your local server use your own chromedriver path
 driver = webdriver.Chrome(DRIVER_PATH)
-driver.get('https://www.instagram.com/p/CGQM1xdB5fW/')
+ig_path = driver.get('https://www.instagram.com/p/CGQM1xdB5fW/')
 
+comment_list = []
 i = 0
-while i < 12:   # instagram only presents 12 comments at a time
-    # append the comment to a list or write to a csv file
+comment_count = 0
+while i < 12:                 # instagram only presents 12 comments at a time
+    comment_list.append(ig_path.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/div[1]/ul/ul[1+'comment_count']/div/li/div/div/div[2]/span'))    # append the comment to a list or write to a csv file
     i += 1
+    comment_count += 1
     if i == 12
-        driver.find_element_by_xpath(#write in the xpath of the button).click()  #the click of this button is to add more comments 
+        ig_path.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[3]/div[1]/ul/li/div/button').click()  #the click of this button is to add more comments 
         i - 12
     else:
         pass
-
-
-   
-
