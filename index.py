b7987403d461517f9c052c6ca2997e5547b67c91
fworkneh7 <fworkneh@umd.edu>
2020-10-27 13:12:42 -0400

    Update index.py

--- index.py
+++ index.py
@@ -4,17 +4,16 @@ DRIVER_PATH = "C:/Users/frazer/Downloads/chromedriver_win32/chromedriver.exe" #w
 driver = webdriver.Chrome(DRIVER_PATH)
 driver.get('https://www.instagram.com/p/CGQM1xdB5fW/')
 
-
-path = r"C:/Users/frazer/OneDrive/Documents/GitHub/assignment-1/insta-scraper/script.csv"
-
-
 i = 0
 while i < 12:   # instagram only presents 12 comments at a time
-  # append the comment to a list or write to a csv file
-  i += 1
-  if i == 12
-    #click button to add more comments 
-    i - 12
+    # append the comment to a list or write to a csv file
+    i += 1
+    if i == 12
+        driver.find_element_by_xpath(#write in the xpath of the button).click()  #the click of this button is to add more comments 
+        i - 12
+    else:
+        pass
+
 
    
 
