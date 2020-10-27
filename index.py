b67f1d0e595cc0e2e7e3a76c25d1e6cf71293ef7
fworkneh7 <fworkneh@umd.edu>
2020-10-27 15:12:04 -0400

    Update index.py

--- index.py
+++ index.py
@@ -6,9 +6,9 @@ ig_path = driver.get('https://www.instagram.com/p/CGQM1xdB5fW/')
 
 comment_list = []
 i = 0
-comment_count = 0
+comment_count = 1
 while i < 12:                 # instagram only presents 12 comments at a time
-    comment_list.append(ig_path.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/div[1]/ul/ul[1+'comment_count']/div/li/div/div/div[2]/span'))    # append the comment to a list or write to a csv file
+    comment_list.append(ig_path.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/div[1]/ul/ul['+ str(comment_count) +']/div/li/div/div/div[2]/span'))    # append the comment to a list or write to a csv file
     i += 1
     comment_count += 1
     if i == 12
