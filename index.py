94eaae8b62744febbed214c400e892405a552efd
fworkneh7 <fworkneh@umd.edu>
2020-12-05 02:50:08 -0500

    Update index.py

--- index.py
+++ index.py
@@ -11,8 +11,10 @@ while i < 12:                 # instagram only presents 12 comments at a time
     comment_list.append(ig_path.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/div[1]/ul/ul['+ str(comment_count) +']/div/li/div/div/div[2]/span'))    # append the comment to a list or write to a csv file
     i += 1
     comment_count += 1
-    if i == 12
+    if i == 12:
         ig_path.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[3]/div[1]/ul/li/div/button').click()  #the click of this button is to add more comments 
         i - 12
     else:
         pass
+
+# print(comment_list)
